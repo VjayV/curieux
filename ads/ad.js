f66bc414bd1b600b5b0e7f5346bf4ba6ab7691a3
vjayv <v95jay@gmail.com>
2021-03-01 16:28:14 +0530

    main boilerplate branch

--- ads/ad.js
+++
@@ -1,9 +0,0 @@
-// const ad=document.querySelector('.ad');
-// const url ='';
-// async function getAd()
-// {
-//     const response = await fetch(url);
-//     const json = await response.json(); 
-//     const image = document.createElement('img');
-//     image.src = json.imageUrl; // 
-// }
